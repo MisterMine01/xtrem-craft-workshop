38f6dc65d5e3591d3ba68f50e6d4c61976521899
wESTWOOD <b.loupiac03@gmail.com>
2023-09-19 09:21:54 +0200

    refactor MoneyTest

--- php/tests/MoneyProblem/MoneyTest.php
+++ php/tests/MoneyProblem/MoneyTest.php
@@ -10,17 +10,21 @@ class MoneyTest extends TestCase
 {
     public function test_add_in_usd_returns_value()
     {
-        $this->assertIsFloat(MoneyCalculator::add(5, Currency::USD(), 10));
-        $this->assertNotNull(MoneyCalculator::add(5, Currency::USD(), 10));
+        $moneyCalculator = MoneyCalculator::add(5, Currency::USD(), 10);
+        $this->assertIsFloat($moneyCalculator);
+        $this->assertNotNull($moneyCalculator);
+
     }
 
     public function test_multiply_in_euros_returns_positive_number()
     {
-        $this->assertLessThan(MoneyCalculator::multiply(10, Currency::USD(), 2), 0);
+        $moneyCalculator = MoneyCalculator::multiply(10, Currency::USD(), 2); 
+        $this->assertLessThan($moneyCalculator, 0);
     }
 
     public function test_divide_in_korean_won_returns_float()
     {
-        $this->assertEquals(MoneyCalculator::divide(4002, Currency::USD(), 4), 1000.5);
+        $moneyCalcultor = MoneyCalculator::divide(4002, Currency::USD(), 4);
+        $this->assertEquals($moneyCalcultor, 1000.5);
     }
 }
