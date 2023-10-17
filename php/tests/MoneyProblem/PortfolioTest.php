fa04b361dda9058f501344b735992d670577ec0f
anna <anna.briancon@etu.u-bordeaux.fr>
2023-10-17 09:35:47 +0200

    COmmentaires sur les règles

--- php/tests/MoneyProblem/PortfolioTest.php
+++ php/tests/MoneyProblem/PortfolioTest.php
@@ -11,6 +11,11 @@ use PHPUnit\Framework\TestCase;
 
 class PortfolioTest extends TestCase
 {
+    /**
+     * Devise identique sans taux de change
+     *
+     * @return void
+     */
     public function test_total_money_in_portfolio_without_exchange_rate()
     {
         // Arrange
@@ -26,6 +31,11 @@ class PortfolioTest extends TestCase
         $this->assertEquals(15, $result);
     }
 
+    /**
+     * Devise différente besoin de taux de change
+     *
+     * @return void
+     */
     public function test_total_money_with_different_in_portfolio_with_exchange_rate()
     {
         // Arrange
@@ -40,7 +50,12 @@ class PortfolioTest extends TestCase
         // Assert
         $this->assertEquals(17, $result);
     }
-    
+
+    /**
+     * Devise différentes mais pas de taux de change
+     *
+     * @return void
+     */
     public function test_total_money_different_current_in_portfolio_without_exchange_rate()
     {
         // Arrange
@@ -56,6 +71,11 @@ class PortfolioTest extends TestCase
         Check::thatCall(fn() => $portfolio->evaluate(Currency::KRW(), $bank))->throws(MissingExchangeRateException::class)->isDescribedBy("USD->KRW");
     }
 
+    /**
+     * Evaluer dans une monnaie qui n'est pas dans le portefeuille
+     *
+     * @return void
+     */
     public function test_evaluate_currency_that_is_not_in_portfolio(){
         // Arrange
         $bank = Bank::create(Currency::EUR(), Currency::KRW(), 1427.16);
