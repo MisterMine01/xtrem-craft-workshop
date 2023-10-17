f3d6e8d3f332e5dbcd1e97428ba687577bf429f1
wESTWOOD <b.loupiac03@gmail.com>
2023-10-17 09:07:46 +0200

    ajout du test compliqué avec addwith0000outcurrency

--- php/tests/MoneyProblem/PortfolioTest.php
+++ php/tests/MoneyProblem/PortfolioTest.php
@@ -5,6 +5,8 @@ namespace Tests\MoneyProblem;
 use MoneyProblem\Domain\Bank;
 use MoneyProblem\Domain\Currency;
 use MoneyProblem\Domain\Portfolio;
+use MoneyProblem\Domain\MissingExchangeRateException;
+use Pitchart\Phlunit\Check;
 use PHPUnit\Framework\TestCase;
 
 class PortfolioTest extends TestCase
@@ -24,7 +26,7 @@ class PortfolioTest extends TestCase
         $this->assertEquals(15, $result);
     }
 
-    public function test_total_money_in_portfolio_with_exchange_rate()
+    public function test_total_money_with_different_in_portfolio_with_exchange_rate()
     {
         // Arrange
         $bank = Bank::create(Currency::EUR(), Currency::USD(), 1.2);
@@ -38,4 +40,26 @@ class PortfolioTest extends TestCase
         // Assert
         $this->assertEquals(17, $result);
     }
+    
+    public function test_total_money_different_current_in_portfolio_without_exchange_rate()
+    {
+        // Arrange
+        $bank = Bank::create(Currency::USD(), Currency::EUR(), 1.2);
+        $portfolio = new Portfolio();
+        $portfolio->add(10, Currency::USD());
+        $portfolio->add(10, Currency::EUR());
+
+        //$this->expectException(\MissingExchangeRateException::class);
+        //$this->expectExceptionMessage("USD->KRW");
+        // Act
+
+        
+        Check::thatCall(fn() => $portfolio->evaluate(Currency::KRW(), $bank))->throws(MissingExchangeRateException::class)->isDescribedBy("USD->KRW");
+       
+        
+        
+    }
+
+
+
 }
