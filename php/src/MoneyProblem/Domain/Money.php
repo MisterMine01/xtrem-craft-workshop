cb06d55cdcd6f28bc6273a3dbb9c2333d9602370
BOUGET Alexandre <abouget68@gmail.com>
2023-10-24 09:34:53 +0200

    divide Money

--- php/src/MoneyProblem/Domain/Money.php
+++ php/src/MoneyProblem/Domain/Money.php
@@ -9,6 +9,9 @@ class Money
 
     public function __construct(float $amount, Currency $currency)
     {
+        if ($amount < 0) {
+            throw new \InvalidArgumentException('Cannot create negative money');
+        }
         $this->amount = $amount;
         $this->currency = $currency;
     }
@@ -19,6 +22,14 @@ class Money
             throw new \InvalidArgumentException('Cannot add money with different currencies');
         }
         return new Money($this->amount + $money->amount, $this->currency);
+    }
+
+    public function divide(Money $money): Money
+    {
+        if ($money->amount == 0) {
+            throw new \LogicException('Cannot divide by zero');
+        }
+        return new Money($this->amount / $money->amount, $this->currency);
     }
 
 }
