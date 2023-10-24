7cc919f4751d81736ea4a6bcc99cb036a5068803
BOUGET Alexandre <abouget68@gmail.com>
2023-10-24 09:33:58 +0200

    test

--- php/tests/MoneyProblem/MoneyTest.php
+++ php/tests/MoneyProblem/MoneyTest.php
@@ -2,14 +2,22 @@
 
 namespace Tests\MoneyProblem;
 
-use http\Exception\InvalidArgumentException;
 use MoneyProblem\Domain\Money;
 use MoneyProblem\Domain\Currency;
 use PHPUnit\Framework\TestCase;
 
 class MoneyTest extends TestCase
 {
-    public function testAddWithSameCurrencies() {
+    public function testCreateNegativeMoney()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Cannot create negative money');
+
+        new Money(-5, Currency::USD());
+    }
+
+    public function testAddWithSameCurrencies()
+    {
         $money = new Money(5, Currency::USD());
 
         $sum = $money->add(new Money(10, Currency::USD()));
@@ -18,13 +26,32 @@ class MoneyTest extends TestCase
         $this->assertEquals($money, new Money(5, Currency::USD()));
     }
 
-    public function testAddDifferentCurrency() {
+    public function testAddDifferentCurrency()
+    {
         $this->expectException(\InvalidArgumentException::class);
         $this->expectExceptionMessage('Cannot add money with different currencies');
 
         $money = new Money(5, Currency::USD());
 
         $money->add(new Money(10, Currency::EUR()));
+    }
 
+    public function testDivideCurrency()
+    {
+        $money = new Money(4002, Currency::KRW());
+
+        $sum = $money->divide(new Money(4, Currency::KRW()));
+
+        $this->assertEquals($sum, new Money(1000.5, Currency::KRW()));
+    }
+
+    public function testDivideZeroCurrency()
+    {
+        $this->expectException(\LogicException::class);
+        $this->expectExceptionMessage('Cannot divide by zero');
+
+        $money = new Money(4002, Currency::KRW());
+
+        $money->divide(new Money(0, Currency::KRW()));
     }
 }
